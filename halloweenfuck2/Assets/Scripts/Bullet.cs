ee2b68fc35680af3ddcadb33c4e9b0324da19f11
Viljami Perheentupa <pviljo@gmail.com>
2019-11-02 12:25:09 +0200

    Halloween2 tehty, vanha poistettu

---
+++ halloweenfuck2/Assets/Scripts/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 2f;
+    void Update()
+    {
+        gameObject.transform.position += Vector3.right * Time.deltaTime * speed;
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+         Destroy(gameObject);
+    }
+}
---
+++ halloweenfuck2/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    public float speed = 1f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position += Vector3.right * Time.deltaTime * speed;        }
+    }
+}
